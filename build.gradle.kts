0076efea47e6d250fa2ab76a0f291b8f80f1479e
Benjamin Gudehus <hastebrot@gmail.com>
2017-08-31 22:35:39 +0200

    (chore) Gradle: Add JUnit5 dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,6 @@
 val kotlinVersion by project
+val junitJupiterVersion by project
+val junitPlatformVersion by project
 
 plugins {
     kotlin("jvm", "1.1.4-3")
@@ -10,4 +12,11 @@ repositories {
 
 dependencies {
     compile(kotlin("stdlib", "$kotlinVersion"))
+}
+
+dependencies {
+    testCompile(kotlin("test", "$kotlinVersion"))
+    testCompile("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
+    testRuntime("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
+    testRuntime("org.junit.platform:junit-platform-launcher:$junitPlatformVersion")
 }
