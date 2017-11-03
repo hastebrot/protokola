b4805d5e17531c8eff8759c9a289db12cce7d154
Benjamin Gudehus <hastebrot@gmail.com>
2017-11-04 00:47:49 +0100

    (refactor) Replace CookieJar with cookies in Request and Response headers.

--- src/main/kotlin/protokola/transport/dolphinChannel.kt
+++ src/main/kotlin/protokola/transport/dolphinChannel.kt
@@ -8,20 +8,14 @@ import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.RequestBody
 import okio.Buffer
+import java.util.concurrent.ConcurrentHashMap
 
 data class HttpRequest(val body: String)
 data class HttpResponse(val status: Int, val body: String)
 
-// cors (cross-origin resource sharing)
-// transport.EndpointUrl()
-// transport.CookieSessionId()
-// transport.DolphinClientId()
-// codec.CreateContext()
-// codec.CreateController()
-
 fun main(args: Array<String>) {
     val client = OkHttpClient.Builder()
-            .cookieJar(simpleCookieJar())
+//            .cookieJar(simpleCookieJar())
             .build()
 
     val dolphinUrl = "http://localhost:8080/dolphin"
@@ -38,20 +32,17 @@ fun main(args: Array<String>) {
         mapOf("id" to "StartLongPoll")
     ).toJson())
 
-    var clientId: String? = null
+    var dolphinClientId: String? = null
+    var cookieSessionId: String? = null
 
     fun fetch(httpRequest: HttpRequest): HttpResponse {
-        val request = makeDolphinRequest(httpRequest.body, dolphinUrl, clientId)
-//        println(request.body()!!.string())
-//        println(request.body()!!.contentType())
-//        println(request.headers())
-
-        val response = client.newCall(request).execute()
-        clientId = response.header(clientIdKey)
-
-        val httpResponse = HttpResponse(response.code(), response.body()!!.string())
-        response.close()
-        return httpResponse
+        val request = makeDolphinRequest(httpRequest.body, dolphinUrl, dolphinClientId, cookieSessionId)
+        val call = client.newCall(request)
+        return call.execute().use { response ->
+            dolphinClientId = response.header(clientIdKey)
+            cookieSessionId = cookieSessionId ?: response.headers(sessionIdKey).firstOrNull()
+            HttpResponse(response.code(), response.body()!!.string())
+        }
     }
 
     println(request1)
@@ -64,12 +55,14 @@ fun main(args: Array<String>) {
     println(fetch(request3))
 }
 
+private val sessionIdKey = "Set-Cookie"
 private val clientIdKey = "dolphin_platform_intern_dolphinClientId"
 
 private fun makeDolphinRequest(
         commands: String,
         endpoint: String = "http://localhost:8080/dolphin",
-        clientId: String? = null): Request {
+        clientId: String? = null,
+        sessionCookie: String? = null): Request {
     val mediaType = MediaType.parse("application/json")
     return Request.Builder().apply {
         url(endpoint)
@@ -78,6 +71,9 @@ private fun makeDolphinRequest(
         if (clientId != null) {
             header(clientIdKey, clientId)
         }
+        if (sessionCookie != null) {
+            addHeader("Cookie", sessionCookie)
+        }
     }.build()
 }
 
@@ -85,15 +81,15 @@ private fun List<*>.toJson() = toString()
 private fun Map<*, *>.toJson() = toString()
 
 private fun simpleCookieJar() = object : CookieJar {
-    private var cookieMap = mutableMapOf<String, List<Cookie>>()
+    private val cookieMap = ConcurrentHashMap<String, List<Cookie>>()
 
     override fun saveFromResponse(url: HttpUrl,
-                                  cookies: List<Cookie>) {
-        cookieMap[url.host()] = cookies
+                                  unmodifiableCookies: List<Cookie>) {
+        cookieMap[url.host()] = unmodifiableCookies.toMutableList()
     }
 
     override fun loadForRequest(url: HttpUrl) =
-        cookieMap[url.host()] ?: listOf()
+        cookieMap[url.host()] ?: mutableListOf()
 }
 
 private fun RequestBody.string() = Buffer()
