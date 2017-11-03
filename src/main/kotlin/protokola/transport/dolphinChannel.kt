35f53cfd54fedf5ed7a3b620fb53f41fcbe6686c
Benjamin Gudehus <hastebrot@gmail.com>
2017-11-03 21:12:46 +0100

    (refactor) Wrap request and response data into messages.

--- src/main/kotlin/protokola/transport/dolphinChannel.kt
+++ src/main/kotlin/protokola/transport/dolphinChannel.kt
@@ -9,6 +9,9 @@ import okhttp3.Request
 import okhttp3.RequestBody
 import okio.Buffer
 
+data class HttpRequest(val body: String)
+data class HttpResponse(val status: Int, val body: String)
+
 // cors (cross-origin resource sharing)
 // transport.EndpointUrl()
 // transport.CookieSessionId()
@@ -20,59 +23,45 @@ fun main(args: Array<String>) {
     val client = OkHttpClient.Builder()
             .cookieJar(simpleCookieJar())
             .build()
+
     val dolphinUrl = "http://localhost:8080/dolphin"
+
+    val request1 = HttpRequest(listOf(
+        mapOf("id" to "CreateContext")
+    ).toJson())
+
+    val request2 = HttpRequest(listOf(
+        mapOf("id" to "CreateController", "n" to "FooController", "c_id" to null)
+    ).toJson())
+
+    val request3 = HttpRequest(listOf(
+        mapOf("id" to "StartLongPoll")
+    ).toJson())
 
     var clientId: String? = null
 
-    run {
-        val jsonString = listOf(
-            mapOf("id" to "CreateContext")
-        ).toJson()
-        val request = makeDolphinRequest(jsonString, dolphinUrl)
-//        println(request.body()!!.string())
-//        println(request.body()!!.contentType())
-
-        val response = client.newCall(request).execute()
-        println(response.code())
-        println(response.body()!!.string())
-//        println(response.header(clientIdKey))
-        clientId = response.header(clientIdKey)
-        response.close()
-    }
-
-    run {
-        val jsonString = listOf(
-            mapOf("id" to "CreateController", "n" to "FooController", "c_id" to null)
-        ).toJson()
-        val request = makeDolphinRequest(jsonString, dolphinUrl, clientId)
+    fun fetch(httpRequest: HttpRequest): HttpResponse {
+        val request = makeDolphinRequest(httpRequest.body, dolphinUrl, clientId)
 //        println(request.body()!!.string())
 //        println(request.body()!!.contentType())
 //        println(request.headers())
 
         val response = client.newCall(request).execute()
-        println(response.code())
-        println(response.body()!!.string())
-//        println(response.header(clientIdKey))
         clientId = response.header(clientIdKey)
+
+        val httpResponse = HttpResponse(response.code(), response.body()!!.string())
         response.close()
+        return httpResponse
     }
 
-    run {
-        val jsonString = listOf(
-            mapOf("id" to "StartLongPoll")
-        ).toJson()
-        val request = makeDolphinRequest(jsonString, dolphinUrl, clientId)
-//        println(request.body()!!.string())
-//        println(request.body()!!.contentType())
-//        println(request.headers())
+    println(request1)
+    println(fetch(request1))
 
-        val response = client.newCall(request).execute()
-        println(response.code())
-        println(response.body()!!.string())
-//        println(response.header(clientIdKey))
-        clientId = response.header(clientIdKey)
-        response.close()
-    }
+    println(request2)
+    println(fetch(request2))
+
+    println(request3)
+    println(fetch(request3))
 }
 
 private val clientIdKey = "dolphin_platform_intern_dolphinClientId"
@@ -81,15 +70,11 @@ private fun makeDolphinRequest(
         commands: String,
         endpoint: String = "http://localhost:8080/dolphin",
         clientId: String? = null): Request {
-//    val mediaType = MediaType.parse("application/json")
-    val mediaType = MediaType.parse("text/plain")
+    val mediaType = MediaType.parse("application/json")
     return Request.Builder().apply {
         url(endpoint)
         post(RequestBody.create(mediaType, commands))
         header("connection", "keep-alive")
-//        header("Access-Control-Allow-Origin", "*")
-//        header("Access-Control-Allow-Headers", "Content-Type")
-//        header("Access-Control-Allow-Methods", "POST")
         if (clientId != null) {
             header(clientIdKey, clientId)
         }
