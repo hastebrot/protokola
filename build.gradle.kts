8a65150bc09aa3863e00f4d5be0690dd94f1a3aa
Benjamin Gudehus <hastebrot@gmail.com>
2017-11-04 12:13:29 +0100

    (feature) Parse JSON to List of Maps.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,14 +5,18 @@ val junitPlatformVersion by project
 buildscript {
     repositories {
         mavenCentral()
+//        maven("https://kotlin.bintray.com/kotlinx")
     }
+
     dependencies {
         classpath("org.junit.platform:junit-platform-gradle-plugin:1.0.1")
+//        classpath("org.jetbrains.kotlinx:kotlinx-gradle-serialization-plugin:0.2")
     }
 }
 
 apply {
     plugin("org.junit.platform.gradle.plugin")
+//    plugin("kotlinx-serialization")
 }
 
 plugins {
@@ -21,10 +25,13 @@ plugins {
 
 repositories {
     jcenter()
+//    maven("https://kotlin.bintray.com/kotlinx")
 }
 
 dependencies {
     compile(kotlin("stdlib", "$kotlinVersion"))
+//    compile(kotlin("reflect", "$kotlinVersion"))
+//    compile("org.jetbrains.kotlinx:kotlinx-serialization-runtime:0.2")
     compile("com.squareup.okhttp3:okhttp:3.9.0")
     compile("com.squareup.moshi:moshi:1.5.0")
     compile("com.squareup.moshi:moshi-kotlin:1.5.0")
--- src/main/kotlin/protokola/transform/dolphinCodec.kt
+++ src/main/kotlin/protokola/transform/dolphinCodec.kt
@@ -1,25 +1,37 @@
 package protokola.transform
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.Types
 import protokola.transport.Transport
 
 fun main(args: Array<String>) {
+    val moshi = Moshi.Builder().build()
+
+    val listOfCommands = Types.newParameterizedType(
+        List::class.java,
+        Map::class.java
+    )
+
+    val jsonAdapter = moshi.adapter<List<Map<String, *>>>(listOfCommands)
+
     val responseContext = Transport.Response(200, """
         [{"p_id":"f6a23423-d104-41a8-84a9-c793ae2cee32","t":"@@@ HIGHLANDER_BEAN @@@","a":[{"n":"@@@ SOURCE_SYSTEM @@@","a_id":"920S","v":"server"},{"n":"controllerName","a_id":"921S","v":null},{"n":"controllerId","a_id":"922S","v":null},{"n":"model","a_id":"923S","v":null}],"id":"CreatePresentationModel"}]
     """.trimIndent())
 
     println(responseContext)
+    println(jsonAdapter.fromJson(responseContext.body))
 }
 
-sealed class DolphinCommand {
-    class CreateContext() : DolphinCommand()
-    class DestroyContext() : DolphinCommand()
+object DolphinCommand {
+    class CreateContext
+    class DestroyContext
 
-    class CreatePresentationModel() : DolphinCommand()
-    class DeletePresentationModel() : DolphinCommand()
+    class CreatePresentationModel
+    class DeletePresentationModel
 
-    class CreateController() : DolphinCommand()
-    class DestroyController() : DolphinCommand()
+    class CreateController
+    class DestroyController
 
-    class ValueChanged() : DolphinCommand()
-    class CallAction() : DolphinCommand()
+    class ValueChanged
+    class CallAction
 }
