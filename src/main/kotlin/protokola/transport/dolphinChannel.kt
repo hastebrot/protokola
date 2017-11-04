c6fbf4803603893df07670f6337a33f3b25e7b5d
Benjamin Gudehus <hastebrot@gmail.com>
2017-11-04 02:47:21 +0100

    (refactor) Use MessageBus for Request and Response.

--- src/main/kotlin/protokola/transport/dolphinChannel.kt
+++ src/main/kotlin/protokola/transport/dolphinChannel.kt
@@ -9,59 +9,65 @@ import okhttp3.Request
 import okhttp3.RequestBody
 import okio.Buffer
 import protokola.Message
+import protokola.MessageBus
 import java.util.concurrent.ConcurrentHashMap
 
 sealed class Transport {
-    data class Request(val body: String) : Transport()
-    data class Response(val status: Int, val body: String): Transport()
+    data class Request(val url: String,
+                       val body: String) : Transport()
+    data class Response(val status: Int,
+                        val body: String) : Transport()
+    data class SessionCookie(val cookie: String) : Transport()
+    data class DolphinClientId(val id: String) : Transport()
 }
 
 fun main(args: Array<String>) {
+    val bus = MessageBus()
+    bus.subscribe { println(it.payload) }
+
     val client = OkHttpClient.Builder()
 //            .cookieJar(simpleCookieJar())
             .build()
 
     val dolphinUrl = "http://localhost:8080/dolphin"
 
-    val request1 = Message(Transport.Request(listOf(
+    val request1 = Message(Transport.Request(dolphinUrl, listOf(
         mapOf("id" to "CreateContext")
     ).toJson()))
 
-    val request2 = Message(Transport.Request(listOf(
+    val request2 = Message(Transport.Request(dolphinUrl, listOf(
         mapOf("id" to "CreateController", "n" to "FooController", "c_id" to null)
     ).toJson()))
 
-    val request3 = Message(Transport.Request(listOf(
+    val request3 = Message(Transport.Request(dolphinUrl, listOf(
         mapOf("id" to "StartLongPoll")
     ).toJson()))
 
     var dolphinClientId: String? = null
     var cookieSessionId: String? = null
 
-    fun fetch(requestMessage: Message<Transport.Request>): Message<Transport.Response> {
-        val request = makeDolphinRequest(requestMessage.payload.body, dolphinUrl, dolphinClientId, cookieSessionId)
+    fun fetch(requestMessage: Message<Transport.Request>) {
+        bus.dispatch(requestMessage)
+        val request = createDolphinRequest(requestMessage.payload.body, requestMessage.payload.url, dolphinClientId, cookieSessionId)
         val call = client.newCall(request)
-        return call.execute().use { response ->
+        call.execute().use { response ->
             dolphinClientId = response.header(clientIdKey)
-            cookieSessionId = cookieSessionId ?: response.headers(sessionIdKey).firstOrNull()
-            Message(Transport.Response(response.code(), response.body()!!.string()))
+            cookieSessionId = cookieSessionId ?: response.headers(setCookieKey).firstOrNull()
+            val responseMessage = Message(Transport.Response(response.code(), response.body()!!.string()))
+            bus.dispatch(responseMessage)
         }
     }
 
-    println(request1.payload)
-    println(fetch(request1).payload)
-
-    println(request2.payload)
-    println(fetch(request2).payload)
-
-    println(request3.payload)
-    println(fetch(request3).payload)
+    fetch(request1)
+    fetch(request2)
+    fetch(request3)
 }
 
-private val sessionIdKey = "Set-Cookie"
+private val cookieKey = "Cookie"
+private val setCookieKey = "Set-Cookie"
 private val clientIdKey = "dolphin_platform_intern_dolphinClientId"
 
-private fun makeDolphinRequest(
+private fun createDolphinRequest(
         commands: String,
         endpoint: String = "http://localhost:8080/dolphin",
         clientId: String? = null,
@@ -75,7 +81,7 @@ private fun makeDolphinRequest(
             header(clientIdKey, clientId)
         }
         if (sessionCookie != null) {
-            addHeader("Cookie", sessionCookie)
+            addHeader(cookieKey, sessionCookie)
         }
     }.build()
 }
