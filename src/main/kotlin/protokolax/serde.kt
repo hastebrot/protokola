8c164759debe39021e4c9d255f902c93637f153d
Benjamin Gudehus <hastebrot@gmail.com>
2018-01-19 23:27:15 +0100

    (feature) Add kotlinx.serialization demo.

---
+++ src/main/kotlin/protokolax/serde.kt
@@ -0,0 +1,26 @@
+package protokolax
+
+import kotlinx.serialization.SerialContext
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.JSON
+
+@Serializable
+data class Data(val id: Int,
+                val str: String)
+
+fun main(args: Array<String>) {
+    val json = JSON(
+        indented = true,
+        context = SerialContext()
+    )
+
+    run {
+        val data = Data(123, "foo")
+        println(json.stringify(data))
+    }
+
+    run {
+        val string = """ { "id": 123, "str": "foo" } """
+        println(json.parse<Data>(string))
+    }
+}
