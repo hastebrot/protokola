df41313ccbec6e8554f93cedfcca03f3cc2667f2
Benjamin Gudehus <hastebrot@gmail.com>
2018-01-30 00:31:58 +0100

    (refactor) Introduce dolphinProperty.

--- src/main/kotlin/protokola/observable/dolphinObservable.kt
+++ src/main/kotlin/protokola/observable/dolphinObservable.kt
@@ -1,15 +1,19 @@
 package protokola.observable
 
 import protokola.demo
+import protokola.property.get
+import protokola.property.pop
+import protokola.property.push
+import protokola.property.set
+import protokola.property.splice
 import kotlin.reflect.KClass
 import kotlin.reflect.KMutableProperty1
-import kotlin.reflect.KProperty1
 
 // we need a plain object with mutable fields.
 data class Person(var firstName: String? = null,
                   var lastName: String? = null)
 
-data class FishShop(val fishes: MutableList<String> = mutableListOf())
+data class FishShop(var fishes: MutableList<String>? = mutableListOf())
 
 fun main(args: Array<String>) {
     demo {
@@ -19,9 +23,9 @@ fun main(args: Array<String>) {
 
         // bind fields of the plain object into observable values.
         bean.property(Person::lastName)
-            .bind(false) { println(it.value) }
+            .bind(true) { println(it.value) }
         bean.property(Person::firstName)
-            .bind(false) { println(it.value) }
+            .bind(true) { println(it.value) }
 
         // change a property value.
         bean.property(Person::lastName).set("Feuerstein")
@@ -38,42 +42,43 @@ fun main(args: Array<String>) {
     demo {
         val bean = Bean(FishShop())
 
-        bean.arrayProperty(FishShop::fishes)
-            .bind(false) { println(it) }
+        bean.property(FishShop::fishes)
+            .bind(true) { println(it.value) }
+        println(bean.instance.fishes)
 
-        bean.arrayProperty(FishShop::fishes)
+        bean.property(FishShop::fishes)
             .push("angel", "clown", "mandarin", "surgeon")
+        println(bean.instance.fishes)
+
+        bean.property(FishShop::fishes)
+            .splice(1, 2, "foo", "bar")
+        println(bean.instance.fishes)
     }
 
 }
 
 // a bean serves as a wrapper for a plain object, that provides access to observable values.
-class Bean<T : Any>(private val instance: T) {
+class Bean<T : Any>(val instance: T) {
 
     private val properties = mutableMapOf<KMutableProperty1<T, *>, Property<T, *>>()
 
     constructor(type: KClass<T>) : this(type.java.newInstance()!!)
 
-    fun <R> property(property: KMutableProperty1<T, R>): Property<T, R> =
+    fun <R : Any?> property(property: KMutableProperty1<T, R?>): Property<T, R> =
         if (property in properties) retrieveProperty(property)
         else addProperty(property)
 
-    fun <R : MutableCollection<V>, V> arrayProperty(
-        property: KProperty1<T, R>): ArrayProperty<T, R, V> {
-        return ArrayProperty(property, instance)
-    }
-
-    fun <R> removeProperty(property: KMutableProperty1<T, R>) {
+    fun <R : Any?> removeProperty(property: KMutableProperty1<T, R?>) {
         properties[property]!!.unbindAll()
         properties.remove(property)
     }
 
-    private fun <R> addProperty(property: KMutableProperty1<T, R>) =
+    private fun <R : Any?> addProperty(property: KMutableProperty1<T, R?>) =
         Property(property, instance)
             .apply { properties[property] = this }
 
     @Suppress("UNCHECKED_CAST")
-    private fun <R> retrieveProperty(property: KMutableProperty1<T, R>) =
+    private fun <R : Any?> retrieveProperty(property: KMutableProperty1<T, R?>) =
         properties[property] as Property<T, R>
 
 }
@@ -82,34 +87,20 @@ class Bean<T : Any>(private val instance: T) {
 inline fun <reified T : Any> bean() = Bean(T::class)
 
 // a property allows to observe value changes via bindings.
-class Property<T, R>(private val property: KMutableProperty1<T, R>,
-                     private val instance: T) {
+class Property<T, R>(val property: KMutableProperty1<T, R?>,
+                     val instance: T) {
 
     private val handlers = mutableListOf<Handler<ValueChange<R>>>()
 
-    private var value: R
-        get() = property.get(instance)
-        set(newValue) {
-            val oldValue = property.get(instance)
-            property.set(instance, newValue)
-            handlers.forEach { handler ->
-                handler(ValueChange(newValue, oldValue))
-            }
-        }
-
-    fun get() = value
-
-    fun set(newValue: R) { value = newValue }
-
     fun bind(initial: Boolean = true,
              handler: Handler<ValueChange<R>>): Binding {
+        handlers += handler
         val handle = {
-            handler(ValueChange(value, null))
+            emit(ValueChange(get(), null))
         }
         if (initial) {
             handle()
         }
-        handlers += handler
         return {
             handlers -= handler
         }
@@ -117,41 +108,34 @@ class Property<T, R>(private val property: KMutableProperty1<T, R>,
 
     fun unbindAll() { handlers.clear() }
 
-}
-
-class ArrayProperty<T, out R: MutableCollection<V>, V>(private val property: KProperty1<T, R>,
-                          private val instance: T) {
-
-    private val handlers = mutableListOf<Handler<ValueSplice<V>>>()
-
-    private var value: Collection<V>
-        get() = property.get(instance)
-        set(newValue) {
-            val oldValue = property.get(instance)
-//            property.set(instance, newValue)
-            handlers.forEach { handler ->
-//                handler(ValueSplice(newValue, oldValue))
-            }
-        }
-
-    fun push(vararg items: V) {
-        (property.get(instance) as MutableCollection<V>).addAll(items)
-    }
-
-    fun bind(initial: Boolean = true,
-             handler: Handler<ValueSplice<V>>): Binding {
-        val handle = {
-            handler(ValueSplice(value.toList(), 0, value.toList(), 0))
-        }
-        if (initial) {
-            handle()
-        }
-        handlers += handler
-        return {
-            handlers -= handler
+    fun emit(valueChange: ValueChange<R>) {
+        handlers.forEach { handler ->
+            handler(valueChange)
         }
     }
+
 }
+
+fun <T, R : Any?> Property<T, R>.get(): R?
+    = get(instance, property)
+
+fun <T, R : Any?> Property<T, R>.set(value: R?) {
+    val oldValue = get(instance, property)
+    set(instance, property, value)
+    emit(ValueChange(value, oldValue))
+}
+
+fun <T, R : MutableList<V>?, V : Any?> Property<T, R>.push(vararg items: V)
+    = push(instance, property, items.toList())
+
+fun <T, R : MutableList<V>?, V : Any?> Property<T, R>.pop()
+    = pop(instance, property)
+
+fun <T, R : MutableList<V>?, V : Any?> Property<T, R>.splice(startIndex: Int,
+                                                             removedCount: Int,
+                                                             vararg addedItems: V)
+    = splice(instance, property, startIndex, removedCount, addedItems.toList())
+
 
 // a binding receives value changes.
 typealias Binding = () -> Unit
---
+++ src/main/kotlin/protokola/property/dolphinProperty.kt
@@ -0,0 +1,31 @@
+package protokola.property
+
+import kotlin.reflect.KMutableProperty1
+import kotlin.reflect.KProperty1
+
+fun <T, R : Any?> get(bean: T,
+                      property: KProperty1<T, R?>): R?
+    = property.get(bean)
+
+fun <T, R : Any?> set(bean: T,
+                      property: KMutableProperty1<T, R?>,
+                      value: R?): Unit
+    = property.set(bean, value)
+
+fun <T, R : MutableList<V>?, V : Any?> push(bean: T,
+                                            property: KProperty1<T, R?>,
+                                            items: Collection<V>): Unit
+    = property.get(bean)!!.run { this!!.addAll(items) }
+
+fun <T, R : MutableList<V>?, V : Any?> pop(bean: T,
+                                           property: KProperty1<T, R?>): V
+    = property.get(bean)!!.run { this!!.removeAt(size - 1) }
+
+fun <T, R : MutableList<V>?, V : Any?> splice(bean: T,
+                                              property: KProperty1<T, R?>,
+                                              startIndex: Int,
+                                              removedCount: Int,
+                                              addedItems: Collection<V>): Unit
+    = property.get(bean)!!.subList(startIndex, startIndex + removedCount)
+        .apply { clear() }
+        .run { addAll(addedItems) }
