7830cceb2b54be9365c58a9007e2048ec42d740b
Benjamin Gudehus <hastebrot@gmail.com>
2018-01-30 02:02:16 +0100

    (feature) Observable: Allow nulls for push() and splice().

--- src/main/kotlin/protokola/property/dolphinProperty.kt
+++ src/main/kotlin/protokola/property/dolphinProperty.kt
@@ -12,20 +12,22 @@ fun <T, R : Any?> set(bean: T,
                       value: R?): Unit
     = property.set(bean, value)
 
+@Suppress("UNCHECKED_CAST")
 fun <T, R : MutableList<V>?, V : Any?> push(bean: T,
                                             property: KProperty1<T, R?>,
-                                            items: Collection<V>): Unit
-    = property.get(bean)!!.run { this!!.addAll(items) }
+                                            items: Collection<V?>): Unit
+    = property.get(bean)!!.run { this!!.addAll(items as Collection<V>) }
 
 fun <T, R : MutableList<V>?, V : Any?> pop(bean: T,
                                            property: KProperty1<T, R?>): V
     = property.get(bean)!!.run { this!!.removeAt(size - 1) }
 
+@Suppress("UNCHECKED_CAST")
 fun <T, R : MutableList<V>?, V : Any?> splice(bean: T,
                                               property: KProperty1<T, R?>,
                                               startIndex: Int,
                                               removedCount: Int,
-                                              addedItems: Collection<V>): Collection<V>
+                                              addedItems: Collection<V?>): Collection<V>
     = property.get(bean)!!.subList(startIndex, startIndex + removedCount)
         .apply { clear() }
-        .apply { addAll(addedItems) }
+        .apply { addAll(addedItems as Collection<V>) }
