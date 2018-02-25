9c3055bc13b3a4d0121def69688d9ea70222158b
Benjamin Gudehus <hastebrot@gmail.com>
2018-02-25 08:24:33 +0100

    (chore) Gradle: Update and add Dokka dependency.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,15 @@
-val kotlinVersion by extra { "1.2.20" }
-val okhttpVersion by extra { "3.9.0" }
+import org.jetbrains.dokka.gradle.DokkaTask
+
+val kotlinVersion by extra { "1.2.21" }
+val kotlinSerialVersion by extra { "0.4.1" }
+val okhttpVersion by extra { "3.9.1" }
 val moshiVersion by extra { "1.5.0" }
-val http4kVersion by extra { "3.13.1" }
+val http4kVersion by extra { "3.17.1" }
 val expektVersion by extra { "0.5.0" }
 
 plugins {
-    kotlin("jvm") version "1.2.20"
+    kotlin("jvm") version "1.2.21"
+    id("org.jetbrains.dokka") version "0.9.16"
 }
 
 buildscript {
@@ -15,7 +19,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("org.jetbrains.kotlinx:kotlinx-gradle-serialization-plugin:0.4")
+        classpath("org.jetbrains.kotlinx:kotlinx-gradle-serialization-plugin:0.4.1")
     }
 }
 
@@ -34,7 +38,7 @@ dependencies {
     compile(kotlin("stdlib-jdk7", kotlinVersion))
     compile(kotlin("stdlib-jdk8", kotlinVersion))
 
-    compile("org.jetbrains.kotlinx:kotlinx-serialization-runtime:0.4")
+    compile("org.jetbrains.kotlinx:kotlinx-serialization-runtime:$kotlinSerialVersion")
     compile("com.squareup.okhttp3:okhttp:$okhttpVersion")
     compile("com.squareup.moshi:moshi:$moshiVersion")
     compile("com.squareup.moshi:moshi-kotlin:$moshiVersion")
@@ -45,6 +49,9 @@ dependencies {
 
     compile("com.squareup:kotlinpoet:0.6.0")
     compile("io.reactivex.rxjava2:rxjava:2.1.9")
+
+    compile("com.canoo.dolphin-platform:dolphin-platform-remoting-server-spring:0.18.0")
+    compile("com.canoo.dolphin-platform:dolphin-platform-remoting-client-javafx:0.18.0")
 }
 
 dependencies {
@@ -52,4 +59,12 @@ dependencies {
     testCompile(kotlin("test-junit", kotlinVersion))
 
     testCompile("com.winterbe:expekt:$expektVersion")
+    testImplementation("com.rubylichtenstein:rxtest:1.0.5")
+}
+
+tasks {
+    "dokkaJavadoc"(DokkaTask::class) {
+        outputFormat = "javadoc"
+        outputDirectory = "$buildDir/javadoc"
+    }
 }
