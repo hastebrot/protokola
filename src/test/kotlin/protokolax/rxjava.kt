4c96d25ef6e123b90f12a939da9bfc705b5995a4
Benjamin Gudehus <hastebrot@gmail.com>
2018-03-10 17:39:52 +0100

    (refactor) Update RxJava example and integrate RxTest.

---
+++ src/test/kotlin/protokolax/rxjava.kt
@@ -0,0 +1,21 @@
+package protokolax
+
+import com.rubylichtenstein.rxtest.assertions.should
+import com.rubylichtenstein.rxtest.assertions.shouldEmit
+import com.rubylichtenstein.rxtest.assertions.shouldHave
+import com.rubylichtenstein.rxtest.extentions.test
+import com.rubylichtenstein.rxtest.matchers.complete
+import com.rubylichtenstein.rxtest.matchers.noErrors
+import io.reactivex.Observable
+
+fun main(args: Array<String>) {
+    Observable.just("Hello world!")
+        .subscribe { println(it) }
+
+    Observable.just("Hello world!")
+        .test {
+            it shouldEmit "Hello world!"
+            it should complete()
+            it shouldHave noErrors()
+        }
+}
